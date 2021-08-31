d08045a7eae9cec1caf6abd7f42dc8fe458ada0e
DevXurde <thexurde123@gmail.com>
2021-08-31 13:26:00 +0530

    v2.0.2

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = "2.0.1"
-DESCRIPTION = "This is a pygame extension to exhibit movement in a few lines for documentation visit https://github.com/DevXurde/pygame_movement_extension"
+VERSION = "2.0.2"
+DESCRIPTION = "This is a pygame extension to exhibit movement in a few lines for documentation visit https://github.com/ZayedMalick/pygame_movement_extension"
 
 classifiers = [
     "Development Status :: 5 - Production/Stable",
@@ -29,6 +29,6 @@ setup(
     keywords=["python", "pygame", "rich-text", "extension",
               "games", "movement", "game-development"],
     classifiers=classifiers,
-    url="https://github.com/DevXurde/pygame_movement_extension",
+    url="https://github.com/ZayedMalick/pygame_movement_extension",
     license="MIT"
 )
